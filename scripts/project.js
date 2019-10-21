1b2f1838d0c87758d851333fd867d5e09b188da4
lfischer27 <laurenfischer@gatech.edu>
2019-10-21 11:47:40 -0400

    updated content

--- scripts/project.js
+++ scripts/project.js
@@ -1,7 +1,9 @@
 var slideIndex = 0;
 var slideIndexv2 = 0;
+var slideIndexv3 = 0;
 showSlides();
 showSlides2();
+showSlides3();
 
 function showSlides() {
   var i;
@@ -25,6 +27,18 @@ function showSlides2() {
   if (slideIndexv2 > slides.length) {slideIndexv2 = 1}    
   slides[slideIndexv2-1].style.display = "block";  
   setTimeout(showSlides2, 2500);
+}
+
+function showSlides3() {
+  var i;
+  var slides = document.getElementsByClassName("GBA");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndexv3++;
+  if (slideIndexv3 > slides.length) {slideIndexv3 = 1}    
+  slides[slideIndexv3-1].style.display = "block";  
+  setTimeout(showSlides3, 2500);
 }
 
 //refrence: https://www.w3schools.com/howto/howto_js_slideshow.asp
