01ac5eebf921bb1778e79989c192958decc41533
lfischer27 <laurenfischer@gatech.edu>
2020-05-28 11:55:15 -0400

    style update

--- scripts/project.js
+++ scripts/project.js
@@ -1,3 +1,31 @@
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidenav").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidenav").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var slideIndex = 0;
 var slideIndexv2 = 0;
 var slideIndexv3 = 0;
